cee303da1422eb0a5699acfe11b075dce6d00651
Sithija Wickramasinghe <2020cs212@stu.ucsc.cmb.ac.lk>
2022-07-17 14:43:27 +0530

    Make changes to files
    Delete Tute1.scala file and added the same file with renaming it.
    Newly added Tutorial3.scala file

---
+++ Tutorial3.scala
@@ -0,0 +1,58 @@
+
+object Tutorial3{
+    def main(args: Array[String]) =
+        {
+
+        }
+
+    //Question 1
+    def areaOfDisk(r: Double):Double = {
+        math.Pi*r*r;
+    }
+    println("Area of Disk: ");
+    printf("%.2f",areaOfDisk(5));
+    println("\n");
+
+    //Question 2
+    def fahrenheit(c:Double):Double = {
+        c*1.80 + 32.0
+    }
+    println("Temperature in Fahrenheit: ");
+    println(fahrenheit(30));
+    println("\n");
+
+    //Question 3
+    def volumeOfSphere(r:Double):Double = {
+        4/3*math.Pi*r*r*r;
+    }
+    println("Volume of Sphere: ");
+    printf("%.2f",volumeOfSphere(5));
+    println("\n");
+
+    //Question 4
+    def bookPrice(x:Int):Double = {
+        x*24.95;
+    }
+
+    def discount(amount:Double):Double = {
+        amount*0.4;
+    }
+
+    def shippingCost(x:Int):Double = {
+        if(x>=50) 50*3  + (x-50)*0.75 else x*3;
+    }
+
+    def totalPrice(x:Int):Double = {
+        bookPrice(x) - discount(bookPrice(x)) + shippingCost(x);
+    }
+    println("Total Price: ");
+    printf("%.2f",totalPrice(60));
+    println("\n");
+
+    //Question 5
+    def pace(x:Double):Double = x*8;
+    def tempo(x:Double):Double = x*7;
+    println("Total running time: ");
+    println(pace(2) + tempo(3) + pace(2) );
+
+}
